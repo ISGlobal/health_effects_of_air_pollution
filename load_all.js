34db1dd3657c656b92ff3c417181f8b848b8794b
arnauuc <arnau.udina@gmail.com>
2016-06-01 10:25:32 +0200

    load js and css

---
+++ load_all.js
@@ -0,0 +1,30 @@
+function loadjscssfile(filename, filetype){
+    if (filetype=="js"){ //if filename is a external JavaScript file
+        var fileref=document.createElement('script')
+        fileref.setAttribute("type","text/javascript")
+        fileref.setAttribute("src", filename)
+    }
+    else if (filetype=="css"){ //if filename is an external CSS file
+        var fileref=document.createElement("link")
+        fileref.setAttribute("rel", "stylesheet")
+        fileref.setAttribute("type", "text/css")
+        fileref.setAttribute("href", filename)
+    }
+    if (typeof fileref!="undefined")
+        document.getElementsByTagName("head")[0].appendChild(fileref)
+}
+
+loadjscssfile("https://fonts.googleapis.com/css?family=Roboto:400,700","css");
+loadjscssfile("./libs/jscrollpane/jquery.jscrollpane.css","css");
+loadjscssfile("style.css","css");
+loadjscssfile("style_nonvw.css","css");
+
+
+
+loadjscssfile("./libs/d3.v3.min.js","js");
+loadjscssfile("http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js","js");
+loadjscssfile("./libs/ijs/i.min.js","js");
+loadjscssfile("./libs/utils.js","js");
+loadjscssfile("./libs/modernizr-custom.js","js");
+loadjscssfile("./libs/jscrollpane/jquery.jscrollpane.min.js","js");
+loadjscssfile("./main.js","js");
--- main.js
+++ main.js
@@ -1,3 +1,36 @@
+d3.select('#health_effects_of_air_pollution').html('<div id="graph">\
+  <h1>Efectos de la contaminación atmosférica sobre la salud</h1>\
+  <div class="tabs">\
+    <div class="cabeza tab">\
+      <h2>Cabeza</h2>\
+        <div class="content"><div></div></div>\
+    </div>\
+    <div class="respiratorio tab">\
+      <h2>Aparato Respiratorio</h2>\
+      <div class="content"><div></div></div>\
+    </div>\
+    <div class="corazon tab">\
+      <h2>Corazón</h2>\
+      <div class="content"><div></div></div>\
+    </div>\
+    <div class="arterias tab">\
+      <h2>Arterias</h2>\
+      <div class="content"><div></div></div>\
+    </div>\
+    <div class="feto tab">\
+      <h2>Feto y placenta</h2>\
+      <div class="content"><div></div></div>\
+    </div>\
+    <div class="reproductivo tab">\
+      <h2>Aparato reproductivo</h2>\
+      <div class="content"><div></div></div>\
+    </div>\
+  </div>\
+</div>')
+
+
+
+
 
 var lang = getParameterByName('lang');
 if(lang!=null && lang!=""){
@@ -11,25 +44,25 @@ if(lang!=null && lang!=""){
 
 
 if (!Modernizr.cssvwunit) {
-  d3.select('body').classed('nonvw',true);
+  d3.select('#health_effects_of_air_pollution').classed('nonvw',true);
 }
 if (!Modernizr.cssvhunit) {
-  d3.select('body').classed('nonvw',true);
+  d3.select('#health_effects_of_air_pollution').classed('nonvw',true);
 }
 
 
 
 
-d3.select('h1').text(iJS._('Efectos de la contaminación atmosférica sobre la salud'));
-d3.select('.cabeza h2').text(iJS._('Cabeza'));
-d3.select('.respiratorio h2').text(iJS._('Aparato Respiratorio'));
-d3.select('.corazon h2').text(iJS._('Corazón'));
-d3.select('.arterias h2').text(iJS._('Arterias'));
-d3.select('.feto h2').text(iJS._('Feto y placenta'));
-d3.select('.reproductivo h2').text(iJS._('Aparato reproductivo'));
+d3.select('#health_effects_of_air_pollution h1').text(iJS._('Efectos de la contaminación atmosférica sobre la salud'));
+d3.select('#health_effects_of_air_pollution .cabeza h2').text(iJS._('Cabeza'));
+d3.select('#health_effects_of_air_pollution .respiratorio h2').text(iJS._('Aparato Respiratorio'));
+d3.select('#health_effects_of_air_pollution .corazon h2').text(iJS._('Corazón'));
+d3.select('#health_effects_of_air_pollution .arterias h2').text(iJS._('Arterias'));
+d3.select('#health_effects_of_air_pollution .feto h2').text(iJS._('Feto y placenta'));
+d3.select('#health_effects_of_air_pollution .reproductivo h2').text(iJS._('Aparato reproductivo'));
 
 
-d3.select('.cabeza .content div').html(iJS._('<p><span class="first"><b>Ictus:</b> 1,4 millones de muertes anuales.</span><br/> \
+d3.select('#health_effects_of_air_pollution .cabeza .content div').html(iJS._('<p><span class="first"><b>Ictus:</b> 1,4 millones de muertes anuales.</span><br/> \
 Según la OMS, un 34% de las muertes prematuras anuales atribuibles a contaminación del aire interior causada por combustibles sólidos se producen por accidentes cerebrovasculares.<br/> \
 <span class="font">Fuente: <a href="http://www.who.int/mediacentre/factsheets/fs292/es/">http://www.who.int/mediacentre/factsheets/fs292/es/</a></span></p> \
 <p><span class="first"><b>Daños en el desarrollo neuronal:</b> Demostrados en animales y en fase de estudio en humanos.</span><br/>\
@@ -39,7 +72,7 @@ Según la OMS, un 34% de las muertes prematuras anuales atribuibles a contaminac
 <p><span class="first"><b>Enfermedades neurodegenerativas:</b> Daños similares a los de un cerebro con Alzheimer.</span><br/>\
 <span class="font">Fuente: J Alzheimers Dis. 2015;45(3):757­70.</span></p>'));
 
-d3.select('.respiratorio .content div').html(iJS._('<p><span class="first"><b>Enfermedad Pulmonar Obstructiva Crónica (EPOC):</b> Cerca de 950.000 muertes anuales.</span></br> \
+d3.select('#health_effects_of_air_pollution .respiratorio .content div').html(iJS._('<p><span class="first"><b>Enfermedad Pulmonar Obstructiva Crónica (EPOC):</b> Cerca de 950.000 muertes anuales.</span></br> \
 Según la OMS, un 22% de las muertes prematuras anuales atribuibles a contaminación del aire interior causada por combustibles sólidos se producen por EPOC. </br>\
 <span class="font">Fuente: <a href="http://www.who.int/mediacentre/factsheets/fs292/es/">http://www.who.int/mediacentre/factsheets/fs292/es/</a></span></p>\
 <p><span class="first"><b>Cáncer de pulmón:</b> Más de 255.000 muertes anuales.</span></br>\
@@ -53,7 +86,7 @@ Según la OMS, un 12% de las muertes prematuras anuales atribuibles a contaminac
 <p><span class="first"><b>Asma:</b> Nuevos casos de asma y agravamiento de los ya existentes.</span></br>\
 <span class="font">Fuente: Respirology. 2012 Aug;17(6):887-98.</span></p>'));
 
-d3.select('.corazon .content div').html(iJS._('<p><span class="first"><b>Infarto de miocardio:</b> Más de 1,1 millón de muertes anuales.</span></br>\
+d3.select('#health_effects_of_air_pollution .corazon .content div').html(iJS._('<p><span class="first"><b>Infarto de miocardio:</b> Más de 1,1 millón de muertes anuales.</span></br>\
 Según la OMS, un 26% de las muertes prematuras anuales atribuibles a contaminación del aire interior causada por combustibles sólidos se producen por infartos de miocardio. </br>\
 <span class="font">Fuente: <a href="http://www.who.int/mediacentre/factsheets/fs292/es/">http://www.who.int/mediacentre/factsheets/fs292/es/</a></span></p>\
 <p><span class="first"><b>Arritmia:</b> Factor desencadenante de arritmias.</span></br>\
@@ -61,7 +94,7 @@ Según la OMS, un 26% de las muertes prematuras anuales atribuibles a contaminac
 <p><span class="first"><b>Fallo congestivo cardíaco:</b> Evidencias consistentes relacionan contaminación atmosférica y mortalidad y morbilidad cardiovascular.</span></br> \
 <span class="font">Fuente: Circulation 2010;121;2331-2378.</span></p>'));
 
-d3.select('.arterias .content div').html(iJS._('<p><span class="first"><b>Hipertensión:</b> Mayor riesgo de hipertensión.</span></br>\
+d3.select('#health_effects_of_air_pollution .arterias .content div').html(iJS._('<p><span class="first"><b>Hipertensión:</b> Mayor riesgo de hipertensión.</span></br>\
 <span class="font">Fuente: Circulation 2010;121;2331-2378.</span></p>\
 <p><span class="first"><b>Inflamación sistémica:</b> Mayor riesgo de inflamación crónica.</span></br>\
 <span class="font">Fuente: Circulation 2010;121;2331-2378.</span></p>\
@@ -71,30 +104,30 @@ Un estudio en la región de Lombardía (Italia) halló que un aumento de la expo
 <p><span class="first"><b>Diabetes:</b> Incremento en el riesgo de diabetes tipos 1 y 2 y de resistencia a la insulina.</span></br>\
 <span class="font">Fuente: <a href="http://www.diabetesandenvironment.org/home/contam/air">http://www.diabetesandenvironment.org/home/contam/air</a></span></p>'));
 
-d3.select('.feto .content div').html(iJS._('<p><span class="first"><b>Nacimiento prematuro:</b> Incremento del riesgo, según múltiples estudios de CREAL-ISGlobal.</span></br>\
+d3.select('#health_effects_of_air_pollution .feto .content div').html(iJS._('<p><span class="first"><b>Nacimiento prematuro:</b> Incremento del riesgo, según múltiples estudios de CREAL-ISGlobal.</span></br>\
 <span class="font">Fuente: Environ Health Perspect; DOI:10.1289/ehp.123-A242.</span></p>\
 <p><span class="first"><b>Peso reducido al nacer:</b> Incremento del riesgo, según múltiples estudios de CREAL-ISGlobal.</span></br>\
 <span class="font">Fuente: Environ Health Perspect; DOI:10.1289/ehp.123-A242.</span></p>\
 <p><span class="first"><b>Preclampsia:</b> Incremento del riesgo, según múltiples estudios de CREAL-ISGlobal.</span></br>\
 <span class="font">Fuente: Hypertension. 2014 Sep;64(3):494-500.</span></p>'));
 
-d3.select('.reproductivo .content div').html(iJS._('<p><span class="first"><b>Reducción en la calidad del esperma:</b> Hipótesis todavía no probada.</span></p>'));
+d3.select('#health_effects_of_air_pollution .reproductivo .content div').html(iJS._('<p><span class="first"><b>Reducción en la calidad del esperma:</b> Hipótesis todavía no probada.</span></p>'));
 
-var w = window,
-    d = document,
-    e = d.documentElement,
-    g = d.getElementsByTagName('body')[0],
-    x = w.innerWidth || e.clientWidth || g.clientWidth,
-    y = w.innerHeight|| e.clientHeight|| g.clientHeight;
+// var w = window,
+//     d = document,
+//     e = d.documentElement,
+//     g = d.getElementsByTagName('body')[0],
+//     x = w.innerWidth || e.clientWidth || g.clientWidth,
+//     y = w.innerHeight|| e.clientHeight|| g.clientHeight;
 
 var scrollpane;
 
 d3.xml("woman.svg", "image/svg+xml", function(error, xml) {
   if (error) throw error;
-  document.body.appendChild(xml.documentElement);
+  document.getElementById("health_effects_of_air_pollution").appendChild(xml.documentElement);
   d3.select('#men').style("visibility", "hidden");
 
-  var svg=d3.select('svg');
+  var svg=d3.select('#health_effects_of_air_pollution svg');
   console.log(svg)
   // svg.attr("height", (d3.select(".tabs").node().getBoundingClientRect().height - 10)+"px");
   // svg.attr("width","auto")
@@ -118,7 +151,7 @@ d3.xml("woman.svg", "image/svg+xml", function(error, xml) {
   // d3.select('.country.sierra_leona').text(iJS._("Sierra Leona"));
   // d3.select('.country.liberia').text(iJS._("Liberia"));
 
-  d3.selectAll('.tab h2').on("click", function(){
+  d3.selectAll('#health_effects_of_air_pollution .tab h2').on("click", function(){
 
     console.log('click')
     var tab= d3.select(this.parentNode)
@@ -128,36 +161,38 @@ d3.xml("woman.svg", "image/svg+xml", function(error, xml) {
     //   .filter(function(d){ console.log(d, this);return d;} )
     //   .style({"display":"none"});
     if(!tab.classed('reproductivo')){
-      d3.select('#men').style("visibility", "hidden");
-      d3.select('#woman').style("visibility", "visible");
+      d3.select('#health_effects_of_air_pollution #men').style("visibility", "hidden");
+      d3.select('#health_effects_of_air_pollution #woman').style("visibility", "visible");
     }else{
-      d3.select('#men').style("visibility", "visible");
-      d3.select('#woman').style("visibility", "hidden");
+      d3.select('#health_effects_of_air_pollution #men').style("visibility", "visible");
+      d3.select('#health_effects_of_air_pollution #woman').style("visibility", "hidden");
 
     }
     var c= tab.attr('class').split(" ")[0]
 
 
     if(!tab.classed("active")){
-      var maxH = d3.select(".tabs").node().getBoundingClientRect().height - tab.select('h2').node().getBoundingClientRect().height;
+      var maxH = d3.select("#health_effects_of_air_pollution .tabs").node().getBoundingClientRect().height - tab.select('h2').node().getBoundingClientRect().height;
       console.log('maxH',maxH)
-      tab.select('.content div').style({
+      tab.select('#health_effects_of_air_pollution .content div').style({
         'height':maxH+"px"
       })
       // scrollpane = $('.'+c+' .content div').jScrollPane();
 
       tab.classed("active", true)
-      d3.selectAll(".tab:not(.active)").classed("hide", true);
-      d3.selectAll("g."+c).classed("active", true);
-      scrollpane = $('.'+c+' .content div').jScrollPane();
+      d3.selectAll("#health_effects_of_air_pollution .tab:not(.active)").classed("hide", true);
+      d3.selectAll("#health_effects_of_air_pollution g."+c).classed("active", true);
+      scrollpane = $('#health_effects_of_air_pollution .'+c+' .content div').jScrollPane({
+          contentWidth: '0px'
+      });
       // console.log()
     }else{
       scrollpane.data('jsp').destroy();
       tab.classed("active", false)
-      d3.selectAll(".tab:not(.active)").classed("hide", false);
-      d3.selectAll("g."+c).classed("active", false);
-      d3.select('#men').style("visibility", "hidden");
-      d3.select('#woman').style("visibility", "visible");
+      d3.selectAll("#health_effects_of_air_pollution .tab:not(.active)").classed("hide", false);
+      d3.selectAll("#health_effects_of_air_pollution g."+c).classed("active", false);
+      d3.select('#health_effects_of_air_pollution #men').style("visibility", "hidden");
+      d3.select('#health_effects_of_air_pollution #woman').style("visibility", "visible");
       resize();
     }
 
@@ -169,26 +204,26 @@ d3.xml("woman.svg", "image/svg+xml", function(error, xml) {
 
   function resize(){
     console.log('resize')
-    var svg=d3.select('svg');
+    var svg=d3.select('#health_effects_of_air_pollution svg');
     // var aspectRatio =  svg.attr('width') / svg.attr('height');
     console.log("d3.selectAll('.tab.active').length",d3.selectAll('.tab.active').length)
-    if(d3.selectAll('.tab.active')[0].length == 0){
+    if(d3.selectAll('#health_effects_of_air_pollution .tab.active')[0].length == 0){
       svg.attr("height", (d3.select(".tabs").node().getBoundingClientRect().height - 10)+"px");
       svg.attr("width",(d3.select(".tabs").node().getBoundingClientRect().height*aspectRatio) +"px")
     }else{
-      var h = (d3.select(".tabs").node().getBoundingClientRect().height + d3.select(".tab.active .content").node().getBoundingClientRect().height - 10)
+      var h = (d3.select("#health_effects_of_air_pollution .tabs").node().getBoundingClientRect().height + d3.select("#health_effects_of_air_pollution .tab.active .content").node().getBoundingClientRect().height - 10)
       svg.attr("height", h+"px");
       svg.attr("width",(h*aspectRatio) +"px")
     }
-    console.log(d3.select(".tabs").node().getBoundingClientRect().height,aspectRatio)
+    console.log(d3.select("#health_effects_of_air_pollution .tabs").node().getBoundingClientRect().height,aspectRatio)
     svg.style({
       // "right": "-" + ((svg.node().getBoundingClientRect().width/2) - (d3.select('g#layer1').node().getBoundingClientRect().width/2))  + "px",
       "right":0,
-      "top": (d3.select('h1').node().getBoundingClientRect().height + 5)+'px'
+      "top": (d3.select('#health_effects_of_air_pollution h1').node().getBoundingClientRect().height + 5)+'px'
     });
     // console.log(((svg.node().getBoundingClientRect().width/2) - (d3.select('g#layer1').node().getBoundingClientRect().width/2)))
-    d3.selectAll('.content').style({
-      "padding-right":d3.select('#layer1').node().getBoundingClientRect().width+"px",
+    d3.selectAll('#health_effects_of_air_pollution .content').style({
+      "padding-right":d3.select('#health_effects_of_air_pollution #layer1').node().getBoundingClientRect().width+"px",
       // "width": d3.select(".tabs").node().getBoundingClientRect().width + "px"
           // "padding-right":"0px"
     })
